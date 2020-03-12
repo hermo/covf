01b05b54e0c47abda9ffb71945751ebec7fa815f
Mirko Grönroos <hermo@dmz.fi>
2020-03-12 14:34:45 +0200

    Add new estimate function for 2020-03-12

--- estimate.js
+++ estimate.js
@@ -1,11 +1,13 @@
 const covf20200310 = x => 1.48284516349*Math.pow(Math.E, 0.252960184179*x)
 const covf20200311 = x => 1.46021640082*Math.pow(Math.E, 0.2560357853*x)
+const covf20200312 = x => 0.0176295582645*Math.pow(x, 4)-0.441513050654*Math.pow(x,3)+3.81783962521*Math.pow(x,2)-11.1483424417*x+11.2604062609
 
 const covfFunctions = {
   '2020-03-10': covf20200310,
-  '2020-03-11': covf20200311
+  '2020-03-11': covf20200311,
+  '2020-03-12': covf20200312,
 }
-
+let fnName = '2020-03-12'
 
 const now = (new Date()).getTime()
 const epoch = (new Date('2020-02-26')).getTime()
@@ -13,7 +15,6 @@ const day = 24*60*60*1e3
 
 const daysSinceStart = Math.ceil((now-epoch)/day)
 
-let fnName = '2020-03-11'
 
 // Allow running simulation using estimate equation for older periods
 if (process.argv[2]) {
