04a1a52cc1b4c83722878a9f712a3ea47e246ad1
Mirko Grönroos <hermo@dmz.fi>
2020-03-23 13:37:43 +0200

    Add estimate for 2020-03-23

--- estimate.js
+++ estimate.js
@@ -1,13 +1,15 @@
 const covf20200310 = x => 1.48284516349*Math.pow(Math.E, 0.252960184179*x)
 const covf20200311 = x => 1.46021640082*Math.pow(Math.E, 0.2560357853*x)
 const covf20200312 = x => 0.0176295582645*Math.pow(x, 4)-0.441513050654*Math.pow(x,3)+3.81783962521*Math.pow(x,2)-11.1483424417*x+11.2604062609
+const covf20200323 = x => -1.63397322274e-3*Math.pow(x, 4)+0.115004075249*Math.pow(x,3)-0.927070788174*Math.pow(x,2)+0.811539293526*x+6.4224384359
 
 const covfFunctions = {
   '2020-03-10': covf20200310,
   '2020-03-11': covf20200311,
   '2020-03-12': covf20200312,
+  '2020-03-23': covf20200323,
 }
-let fnName = '2020-03-12'
+let fnName = '2020-03-23'
 
 const now = (new Date()).getTime()
 const epoch = (new Date('2020-02-26')).getTime()
